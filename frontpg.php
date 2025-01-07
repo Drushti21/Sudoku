03793f65807dbd47f1d69cc325fcd0eeee14a1de
Drushti21 <87225107+Drushti21@users.noreply.github.com>
2025-01-07 21:36:33 +0530

    Update and rename index.html to frontpg.php

---
+++ frontpg.php
@@ -0,0 +1,26 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="stylepg.css">
+    <style>
+@import url('https://fonts.googleapis.com/css2?family=Agbalumo&family=Amita:wght@400;700&family=Bree+Serif&family=DM+Serif+Text:ital@0;1&family=Inconsolata:wght@500&family=Josefin+Sans:wght@500&family=Nunito:ital,wght@0,200;0,300;0,400;0,500;1,200;1,300;1,400&family=Poppins:ital,wght@1,100&display=swap');
+</style>
+    <title>SUDOKU</title>
+  </head>
+  <body>
+    <div class="frontbt">
+    <h1 class="text-center">SUDOKU</h1>
+    <h3 class="text-center">Select your level</h3>
+    <button class="btn">Easy</button>
+    <button class="btn">Medium</button>
+    <button class="btn">Hard</button>
+</div>
+</body>
+</html>
